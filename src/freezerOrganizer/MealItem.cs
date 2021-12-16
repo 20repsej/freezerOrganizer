065e8eccd5fc2944f65ab72dbe412b85ff36532a
Jesper Berg <jesper.berg@mailbox.org>
2021-12-16 18:29:54 +0100

    More backend and css

--- src/freezerOrganizer/MealItem.cs
+++ src/freezerOrganizer/MealItem.cs
@@ -1,8 +1,9 @@
-public class FreezerItem
+public class MealItem
 {
     public string? Title { get; set; }
     public bool Alive { get; set; }
     public DateTime Date { get; set; } 
     public string? Description { get; set; }
     public bool completeMeal { get; set; }
+    public string? MealSize { get; set; }
 }
